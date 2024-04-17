4c7903849ceca2f985f84e9d54f724b5c045b217
gulsah <gmutluayyigit@gmail.com>
2024-04-17 21:04:20 +0300

    .

---
+++ src/components/Typografy.jsx
@@ -0,0 +1,30 @@
+
+import React from 'react'
+
+const Typografy = () => {
+    return (
+        <div>
+            <h3 className='text-2xl'>Typografy</h3>
+            <p className='text-sm'>Typografy</p>
+            <p className='text-lg'>Typografy</p>
+            <p className='text-xl'>Typografy</p>
+            <p className='text-2xl'>Typografy</p>
+            <p className='text-3xl'>Typografy</p>
+            <p className='text-base/10'>Typografy</p>
+            
+            <article>
+                <p className='line-clamp-1'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat unde cumque quasi iste reprehenderit architecto recusandae animi aut autem doloremque.</p>
+            </article>
+
+            <p className='text-center'>Merhaba</p>
+            <p className='underline'>Merhaba</p>
+            <p className='line-through'>Merhaba</p>
+            <p className='uppercase'>Merhaba</p>
+            <p className='lowercase'>Merhaba</p>
+            
+            
+                    </div>
+    )
+}
+
+export default Typografy

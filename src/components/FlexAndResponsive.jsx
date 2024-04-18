48d6eaa1b39e9d33c71699ece3d7f8738e9f0c18
gulsah <gmutluayyigit@gmail.com>
2024-04-18 22:41:04 +0300

    flex

--- src/components/FlexAndResponsive.jsx
+++ src/components/FlexAndResponsive.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 
 const FlexAndResponsive = () => {
+    const arr = [1,2,3,4,5,6,7,8]
   return (
     <>
     <div className='flex flex-row flex-wrap'>
@@ -11,6 +12,15 @@ const FlexAndResponsive = () => {
         <div className=' basis-1/4 bg-fuchsia-500'>04</div>
         <div className=' basis-2/4 bg-fuchsia-600'>05</div>
         <div className=' basis-2/4 bg-fuchsia-700'>06</div>
+    </div>
+
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+        {arr.map((item, i)=>(
+            <div key={i} className='bg-teal-500'>
+                {i + 1}
+            </div>
+        )
+    )}
     </div>
 
 
